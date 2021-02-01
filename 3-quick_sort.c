79f1f99be1652aee0c0c3570f0e5b2510e9cbf4c
Diego Lopez <dfelo93@gmail.com>
2021-02-01 11:27:40 -0500

    Updated conditions for printing.

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,47 +1,89 @@
 #include "sort.h"
 /**
+ * quick_sort - Sorts an array of integers, in          *
+ *               ascending order.                       *
+ *                                                      *
+ *  Arguments:                                          *
+ *    @array:  - Pointer to array of ints.              *
+ *     @size:  - Size of the array of integers.         *
+ *       _____   __  ______  ______  ______             *
+ *      /\  __-./\ \/\  ___\/\  ___\/\  __ \            *
+ *      \ \ \/\ \ \ \ \  __\\ \ \__ \ \ \/\ \           *
+ *       \ \____-\ \_\ \_____\ \_____\ \_____\          *
+ *        \/____/ \/_/\/_____/\/_____/\/_____/          *
+ *       __      ______  ______  ______  ______         *
+ *      /\ \    /\  __ \/\  == \/\  ___\/\___  \        *
+ *      \ \ \___\ \ \/\ \ \  _-/\ \  __\\/_/  /__       *
+ *       \ \_____\ \_____\ \_\   \ \_____\/\_____\      *
+ *        \/_____/\/_____/\/_/    \/_____/\/_____/      *
+ *                                                      *
+ *                      Jan-2021                        *
  */
-#include <unistd.h>
 void quick_sort(int *array, size_t size)
 {
+	quick__sort(array, size, size, array);
+}
+/**
+ * quick__sort - Auxiliary function for recursion in
+ *                quick_sort.
+ *
+ *  Arguments:
+ *   @array:   - Pointer to array of integers.
+ *    @size:   - Size of the array.
+ *  @tot_size: - Total size of the array.
+ *   @saved:   - Pointer saving the address of the
+ *                 beggining of the array.
+ *
+ */
+void quick__sort(int *array, size_t size, size_t tot_size, int *saved)
+{
 	unsigned int pivot = 0;
-	int *ptr = array;
-
 
 	if (size > 1)
 	{
-		pivot = partition(array, pivot, size - 1);
-		print_array(ptr, 10);
-		quick_sort(array + pivot, size - pivot);
+		pivot = partition(array, pivot, size - 1, saved, tot_size);
+		quick__sort(array + pivot, size - pivot, tot_size, saved);
 		if (pivot)
-		{
-			quick_sort(array, pivot++);
-		}
+			quick__sort(array, pivot++, tot_size, saved);
 	}
 }
 /**
+ * partition  - Function to divide the array and return a pivot,
+ *                the left side of the array from pivot is less
+ *                than the pivot, the same for the right side
+ *                after the pivot.
+ *  Arguments:
+ *   @array:  - Pointer to current array to divide.
+ *    @low:   - Index for the low limit of the array.
+ *    @hig:   - Index for the high limit of the array.
+ *    @ptr:   - Pointer to the start of the array.
+ *    @size:  - Size of the array to print.
+ *
+ *   Return:  - Index of the new pivot.
  */
-unsigned int partition(int *array, unsigned int low, unsigned int hig)
+size_t partition(int *array, size_t low, size_t hig, int *ptr, size_t size)
 {
 	int swp = 0, pivot = array[hig];
-	unsigned int i = low, j = low;
-	char *green = "\033[92m", *reset = "\033[0m";
+	size_t i = low, j = low;
 
-	for (j = low; j < hig; j++)
+	for (j = low; j <= hig; j++)
 		if (array[j] < pivot)
 		{
-			swp = array[j];
-			array[j] = array[i];
-			array[i] = swp;
+			if (array[j] != array[i])
+			{
+				swp = array[j];
+				array[j] = array[i];
+				array[i] = swp;
+				print_array(ptr, size);
+			}
 			i++;
-			sleep(1);
-			printf("---array[0]--->%s%d%s\n", green, array[0], reset);
-			printf("---array[1]--->%s%d%s\n", green, array[1], reset);
-			printf("---array[2]--->%s%d%s\n", green, array[2], reset);
-			printf("~~~~~~~~~~~~~~~~~~~~\n");
 		}
-	swp = array[hig];
-	array[hig] = array[i];
-	array[i] = swp;
+	if (array[i] != array[hig])
+	{
+		swp = array[hig];
+		array[hig] = array[i];
+		array[i] = swp;
+		print_array(ptr, size);
+	}
 	return (i);
 }
