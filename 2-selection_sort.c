cbca45c962722f12a49342a78ddc963c1829266a
Diego Lopez <dfelo93@gmail.com>
2021-02-01 00:32:47 -0500

    Removed un-needed if condition on line 28.

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -25,21 +25,20 @@ void selection_sort(int *array, size_t size)
 	unsigned int i = 0, j = 0, idx = 0;
 	int swp;
 
-	if (size > 1)
-		for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
+	{
+		idx = i;
+
+		for (j = i + 1; j < size; j++)
+			if (array[j] < array[idx])
+				idx = j;
+
+		if (idx != i)
 		{
-			idx = i;
-
-			for (j = i + 1; j < size; j++)
-				if (array[j] < array[idx])
-					idx = j;
-
-			if (idx != i)
-			{
-				swp = array[i];
-				array[i] = array[idx];
-				array[idx] = swp;
-				print_array(array, size);
-			}
+			swp = array[i];
+			array[i] = array[idx];
+			array[idx] = swp;
+			print_array(array, size);
 		}
+	}
 }
