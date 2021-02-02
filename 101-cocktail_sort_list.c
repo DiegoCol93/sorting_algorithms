53ec535b5f4e131da163f5a76142afcfb248b891
Diego Lopez <dfelo93@gmail.com>
2021-02-02 11:24:08 -0500

    Fixed betty style, added swp_fwd and swp_bck functions.

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -9,60 +9,94 @@
 #include <unistd.h>
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *cmp = NULL, *mov = NULL;
+	listint_t *current = NULL;
 	int swapped = 1;
 
 	if (list)
 	{
-		cmp = *list;
+		current = *list;
 		while (swapped)
 		{
 			swapped = 0;
-			while (cmp->next)
+			while (current->next)
 			{
-				if (cmp->n > cmp->next->n)
+				if (current->n > current->next->n)
 				{
-					mov = cmp->next; /* Start moving */
-					mov->prev = cmp->prev;/*  */
-					if (cmp->prev)
-						cmp->prev->next = mov;
-					else
-						*list = cmp->next;
-					cmp->next = mov->next;
-					cmp->prev = mov;
-					mov->next = cmp;
-					if (cmp->next)
-						cmp->next->prev = cmp;
-					swapped = 1, print_list(*list);
+					swapped = swp_fwd(current, list);
 					continue;
 				}
-				cmp = cmp->next;
+				current = current->next;
 			}
 			if (!swapped)
 				break;
 			swapped = 0;
-			while (cmp->prev)
+			while (current->prev)
 			{
-				if (cmp->n < cmp->prev->n)
+				if (current->n < current->prev->n)
 				{
-					mov = cmp->prev;
-					/* Set ptrs of the next after cmp.*/
-					mov->next = cmp->next;
-					cmp->next->prev = mov;
-
-					/* Set ptrs before mov. */
-					cmp->prev = mov->prev;
-					cmp->next = mov;
-					mov->prev = cmp;
-					if (cmp->prev)
-						cmp->prev->next = cmp;
-					else
-						*list = cmp;
-					swapped = 1, print_list(*list);
+					swapped = swp_bck(current, list);
 					continue;
 				}
-				cmp = cmp->prev;
+				current = current->prev;
 			}
 		}
 	}
+}
+/**
+ * swp_fwd    - Swaps forward two nodes of a linked list.
+ *
+ * Arguments:
+ *  @current: - Pointer to the current node to swap.
+ *   @list:   - Popinter to the list of nodes.
+ *
+ *  Return:     1, Always.
+ */
+int swp_fwd(listint_t *current, listint_t **list)
+{
+	listint_t *moving = NULL;
+
+	moving = current->next;
+
+	moving->prev = current->prev;
+	if (current->prev)
+		current->prev->next = moving;
+	else
+		*list = current->next;
+
+	current->next = moving->next;
+	current->prev = moving;
+	moving->next = current;
+	if (current->next)
+		current->next->prev = current;
+
+	print_list(*list);
+	return (1);
+}
+/**
+ * swp_bck    - Swaps backward two nodes of a linked list.
+ *
+ * Arguments:
+ *  @current: - Pointer to the current node to swap.
+ *   @list:   - Popinter to the list of nodes.
+ *
+ *  Return:     1, Always.
+ */
+int swp_bck(listint_t *current, listint_t **list)
+{
+	listint_t *moving = NULL;
+
+	moving = current->prev;
+
+	moving->next = current->next;
+	current->next->prev = moving;
+
+	current->prev = moving->prev;
+	current->next = moving;
+	moving->prev = current;
+	if (current->prev)
+		current->prev->next = current;
+	else
+		*list = current;
+	print_list(*list);
+	return (1);
 }
